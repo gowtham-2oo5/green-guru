e9e1fa4b484b9c56f1e317aa69d4b469df2d6f22
gowtham-2oo5 <gowtham.ala12345@gmail.com>
2024-03-16 18:59:47 +0530

    Updated UserModel

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -20,6 +20,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  phone: {
+    type: String,
+    required: true,
+  },
   age: {
     type: String,
     required: true,
